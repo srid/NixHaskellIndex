bc228793279693db7adef025e764c7f45d9fa43e
Sridhar Ratnakumar <srid@srid.ca>
2022-11-16 22:45:10 -0500

    lower case data

--- src/NHI/data.nix
+++ src/NHI/data.nix
@@ -1,6 +1,6 @@
 # Nix to generate data to be parsed into NHI/Types.hs
 { pkgs, lib }:
-builtins.groupBy (x: x.pname)
+builtins.groupBy (x: lib.toLower x.pname)
   (lib.mapAttrsToList (n: v: { name = n; inherit (v) pname version; })
     (lib.filterAttrs (n: v: builtins.typeOf v == "set" && lib.hasAttr "pname" v)
       pkgs.haskellPackages))
